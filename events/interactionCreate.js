952ac655e46a57691e9d820f448baa2f112b408f
cmasta9 <141727846+cmasta9@users.noreply.github.com>
2024-03-17 00:39:35 -0500

    Update interactionCreate.js
    A lot more event handling, including support for the cooldown timer, updated status check, and actions made without a pet.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,5 +1,7 @@
 const { Events } = require('discord.js');
-const { StatusCheck } = require('../StatusCheck.js');
+const { statusCheck } = require('../StatusCheck.js');
+const {cooldownCheck} = require('../CooldownCheck.js');
+const { pets } = require('../database.js');
 
 module.exports = {
 	name: Events.InteractionCreate,
@@ -14,12 +16,34 @@ module.exports = {
 		}
 
 		try {
-			//pet status check
-			if(await StatusCheck==-1){
-				await interaction.reply('Your Discotchi ran away to find a better home!')
-			}else{
-				await command.execute(interaction);
+			const has = await pets.has(interaction.user.id);
+			if(has){
+				const cd = await cooldownCheck(interaction.user.id);
+				const status = await statusCheck(interaction.user.id,command.data.name,cd);
+				if(status == -1){
+					await interaction.reply('Your Disgotchi ran away to find a better home!');
+				}else{
+					if(status == 2){
+						await interaction.channel.send(`<@${interaction.user.id}>, your pet leveled up!`);
+					}
+					if(cd <= 0 || command.data.name == "show"){
+						await command.execute(interaction);
+					}
+					else{
+						console.log(`cooldown: ${cd} seconds`);
+						await interaction.reply(`You need to wait ${Math.round(cd)} seconds before you can do anything else.`);
+					}
+				}
 			}
+			else{
+				if(command.data.name != 'adopt'){
+					interaction.reply("You must adopt a pet before you can do anything else.");
+				}
+				else{
+					await command.execute(interaction);
+				}
+			}
+			
 			
 		} catch (error) {
 			console.error(error);
