70d4f992f41761511a5014ce4286fbf5a87583ee
cmasta9 <141727846+cmasta9@users.noreply.github.com>
2024-03-17 00:42:44 -0500

    Update Pet.js
    added cooldown and removed busy boolean

--- Pet.js
+++ Pet.js
@@ -1,26 +1,13 @@
-/*
-- busy : Boolean
-- timeBusy: DateTime (UTC)
-- happiness: Integer (0 - 10)
-- hunger: Integer (0 - 10)
-- thirst: Integer (0 - 10)
-- energy: Integer (0 - 100)
-- cleanliness: Integer (0 - 10)
-- xp: Integer ( 0 - no cap)
-- level: Integer ( 0 - 10?)
-*/
-
 module.exports = class Pet {
-    constructor(type, nickname, busy=false, happiness=7, hunger=7, thirst=7, energy=100, cleanliness=7, exp=0, lvl=1, food=0, water=0, cooldown=0) {
+    constructor(type, nickname, happiness=7, hunger=7, thirst=7, energy=100, cleanliness=7, exp=0, lvl=1, food=0, water=0, cooldown=0) {
         this.nickname = nickname;           // Set by the user
-        this.busy = busy;                   // boolean
         this.happiness = happiness;         // 0 - 10
         this.hunger = hunger;               // 0 - 10
         this.thirst = thirst;               // 0 - 10
         this.energy = energy;               // 0 - 100
         this.cleanliness = cleanliness;     // 0 - 10
-        this.exp = exp;                     // 0 - no cap
-        this.lvl = lvl;                     // 0 - 30
+        this.exp = exp;                     // 0 - 100
+        this.lvl = lvl;                     // 0 - no cap
         this.food = food;
         this.water = water;
         this.cooldown = cooldown;
@@ -46,5 +33,3 @@ module.exports = class Pet {
     }   
 
 }
-
-
