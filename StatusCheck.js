d0ed5410f2c99e01f8d770f1294b076c103f489f
cmasta9 <141727846+cmasta9@users.noreply.github.com>
2024-03-17 00:45:24 -0500

    Update StatusCheck.js
    fixed exp handling, added support for cleanliness and leveling up

--- StatusCheck.js
+++ StatusCheck.js
@@ -1,16 +1,27 @@
 const { pets } = require('./database.js');
-async function check(userId){
+
+//various maintenance functions before any command executes
+async function statusCheck(userId,command,cooldown){
     let inPet = await pets.get(userId);
-    if(inPet.exp >= 100){
-        inPet.lvl+=1;
-    }else{
-        inPet.exp+=15;
-    }
     if(inPet.happiness<=-5 && (inPet.hunger<=0 || inPet.thirst<=0 || inPet.energy<=-50)){
         await pets.delete(userId);
         return -1;
     }
+    if(inPet.cleanliness <= 0){
+        inPet.happiness -= 1;
+        await pets.set(userId,inPet);
+    }
+    if(command != 'show' && cooldown <= 0){
+        inPet.exp += 10; //only gives exp when pet is ready to perform an action
+        await pets.set(userId,inPet);
+    }
+    if(inPet.exp >= 100){
+        inPet.lvl+=1;
+        inPet.exp -= 100;
+        await pets.set(userId,inPet);
+        return 2;
+    }
     return 1;
 }
 
-module.exports = {check};
+module.exports = {statusCheck};
